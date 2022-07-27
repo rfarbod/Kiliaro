27b912ccfc24c83eb92c2453571b65c2306ef272
rfarbod <rfarbod@icloud.com>
2022-07-27 15:10:37 +0430

    First commit

---
+++ KiliaroTest/KiliaroTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiliaroTest
+//
+//  Created by Farbod Rahiminik on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
